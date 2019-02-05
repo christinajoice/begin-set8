30f9eb15d15e2bd2776cf0b172fa7607c31adddc
christinajoice <christinallrejoice@gmail.com>
2019-02-04 16:20:06 -0800

    Update Vowel.py

--- Vowel.py
+++ Vowel.py
@@ -1,6 +1,6 @@
 a=input("Enter a string")
 for x in a:
-  if(x=='a' or x=='e' or x=='i' or x=='o' or x=='u')::
+  if(x=='a' or x=='e' or x=='i' or x=='o' or x=='u'):
     print("yes")
     break;
   else:
