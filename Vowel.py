9956d68fa1f31c5186e284601030c9aae7bcd80d
christinajoice <christinallrejoice@gmail.com>
2019-06-14 14:37:15 +0530

    Update Vowel.py

--- Vowel.py
+++ Vowel.py
@@ -1,7 +1,7 @@
-a=input("Enter a string")
+a=input()
+li=['a','e','i','o','u']
 for x in a:
-  if(x=='a' or x=='e' or x=='i' or x=='o' or x=='u'):
+  if x in li:
     print("yes")
     break;
-  else:
-    print("no")
+print("no")
