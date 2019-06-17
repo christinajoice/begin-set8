b0770e54f10c5e54ceab8d016bbce0a6a6d6e533
christinajoice <christinallrejoice@gmail.com>
2019-06-17 12:44:49 +0530

    Update Composite.py

--- Composite.py
+++ Composite.py
@@ -1,8 +1,11 @@
-n=int(input("Enter a number"))
-if n>1:
-  for i in range(2,n):
-    if (n%i)==0:
-      print("composite");
-    else:
-      print("not composite"):
+n=int(input())
+flag=0
+for i in range(2,n):
+  if (n%i)==0:
+    flag=1
+    break
+  if flag==1:
+    print("yes")
+  else:
+    print("no")
   
